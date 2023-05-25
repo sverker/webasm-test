291e9ef183cdd86e66493d0016b23b1e80b9e021
Sverker Eriksson <sverker@erlang.org>
2023-05-25 18:37:11 +0200

    clenup add.c

--- add.c
+++ add.c
@@ -4,74 +4,31 @@
 #include "erl_nif_wasm.h"
 
 
-static int32_t global_static = 100;
-static int32_t global_extern = 200;
-
-void inc_data()
+int32_t inc_static_data()
 {
-  static int32_t local_static = 300;
-  global_static++;
-  global_extern++;
-  local_static++;
-  enif_printf_I("\nglobal_static = %d", global_static);
-  enif_printf_I("\nglobal_extern = %d", global_extern);
-  enif_printf_I("\nlocal_static = %d\n", local_static);
+  static int32_t local_static = 0;
+
+  return ++local_static;
 }
 
-int32_t add (int32_t first, int32_t second)
+int32_t add_I32(int32_t first, int32_t second)
 {
-  int32_t ret1 = enif_printf_I("add: first  = %x\n", first);
-  int32_t ret2 = enif_printf_I("add: second = %x\n", second);
-  enif_printf_I("add: sizeof(long) = %d\n", sizeof(long));
-  enif_printf_I("add: ret1 = %x\n", ret1);
-  enif_printf_I("add: ret2 = %x\n", ret2);
   return first + second;
 }
 
 
-int64_t addL (int64_t first, int64_t second)
+int64_t add_I64(int64_t first, int64_t second)
 {
-  int64_t ret1 = enif_printf_L("addL: first  = %lx\n", first);
-  int64_t ret2 = enif_printf_L("addL: second = %lx\n", second);
-  enif_printf_L("addL: ret1 = %lx\n", ret1);
-  enif_printf_L("addL: ret2 = %lx\n", ret2);
   return first + second;
 }
 
-double addF (double first, double second)
+double add_F64(double first, double second)
 {
-  enif_printf_F("addF: first = %f\n", first);
-  enif_printf_F("addF: second = %f\n", second);
   return first + second;
 }
 
-int32_t dirty_read(int64_t native_address)
-{
-  int32_t *ptr = (int32_t*)native_address;
-  int32_t value;
 
-  enif_printf_L("dirty_read: native_address=%lx\n", native_address);
-  value = *ptr;
-  enif_printf_L("dirty_read: value=%x\n", value);
-  return value;
-}
-
-
-char* buffy(char* buf, int32_t size)
-{
-  int i;
-  for (i=0; i < size; i++)
-    buf[i] = ~buf[i];
-  return buf;
-}
-
-void segv(void)
-{
-  *(int*)17 = 42;
-}
-
-
-ERL_NIF_TERM my_nif(ErlNifEnv env, ERL_NIF_TERM arg1, ERL_NIF_TERM arg2)
+ERL_NIF_TERM add_terms(ErlNifEnv env, ERL_NIF_TERM arg1, ERL_NIF_TERM arg2)
 {
   int32_t a1 = -666;
   int32_t a2 = -777;
@@ -83,7 +40,7 @@ ERL_NIF_TERM my_nif(ErlNifEnv env, ERL_NIF_TERM arg1, ERL_NIF_TERM arg2)
   return enif_wasm_make_int32(env, a1+a2);
 }
 
-ERL_NIF_TERM add_list(ErlNifEnv env, ERL_NIF_TERM list)
+ERL_NIF_TERM add_list_terms(ErlNifEnv env, ERL_NIF_TERM list)
 {
   ERL_NIF_TERM head, tail;
   int32_t value, sum = 0;
@@ -99,3 +56,5 @@ ERL_NIF_TERM add_list(ErlNifEnv env, ERL_NIF_TERM list)
   }
   return enif_wasm_make_int32(env, sum);
 }
+
+
